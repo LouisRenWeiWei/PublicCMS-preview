2e8b9cf88539a585cd95c79a8860e0c741dd6cac
sanluan <master@sanluan.com>
2018-04-08 21:01:00 +0800

    ipv6兼容性优化

--- publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V4.0.20180210-preview.sql
+++ publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V4.0.20180210-preview.sql
@@ -1,1 +1,4 @@
 UPDATE `sys_module` SET `authorized_url` = 'sysUser/lookup,cmsContent/recycle,cmsContent/realDelete' WHERE  `id` = 117;
+ALTER TABLE `sys_user` MODIFY COLUMN `last_login_ip` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后登录ip' AFTER `last_login_date`;
+ALTER TABLE `sys_user_token` MODIFY COLUMN `login_ip` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登陆IP' AFTER `create_date`;
+
