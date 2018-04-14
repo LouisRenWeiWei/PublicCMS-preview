eb86aede6e045a8e36ece4a965cfc5a461b60632
sanluan <zyyy358@126.com>
2018-04-14 16:26:51 +0800

    删除home前缀用不到的表和功能

--- publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V4.0.20180210-preview.sql
+++ publiccms-parent/publiccms-core/src/main/resources/initialization/upgrade/V4.0.20180210-preview.sql
@@ -1,4 +1,24 @@
 UPDATE `sys_module` SET `authorized_url` = 'sysUser/lookup,cmsContent/recycle,cmsContent/realDelete' WHERE  `id` = 117;
 ALTER TABLE `sys_user` MODIFY COLUMN `last_login_ip` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '最后登录ip' AFTER `last_login_date`;
 ALTER TABLE `sys_user_token` MODIFY COLUMN `login_ip` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登陆IP' AFTER `create_date`;
-
+-- 20180414 --
+DROP TABLE IF EXISTS `home_active`;
+DROP TABLE IF EXISTS `home_article`;
+DROP TABLE IF EXISTS `home_article_content`;
+DROP TABLE IF EXISTS `home_attention`;
+DROP TABLE IF EXISTS `home_broadcast`;
+DROP TABLE IF EXISTS `home_comment`;
+DROP TABLE IF EXISTS `home_comment_content`;
+DROP TABLE IF EXISTS `home_directory`;
+DROP TABLE IF EXISTS `home_file`;
+DROP TABLE IF EXISTS `home_friend`;
+DROP TABLE IF EXISTS `home_friend_apply`;
+DROP TABLE IF EXISTS `home_group`;
+DROP TABLE IF EXISTS `home_group_apply`;
+DROP TABLE IF EXISTS `home_group_post`;
+DROP TABLE IF EXISTS `home_group_post_content`;
+DROP TABLE IF EXISTS `home_group_user`;
+DROP TABLE IF EXISTS `home_score`;
+DROP TABLE IF EXISTS `home_user`;
+UPDATE `sys_module` SET `parent_id` = 149 WHERE  `id` in (150,151,152,153,154);
+UPDATE `sys_module` SET `parent_id` = 99 WHERE  `id` in (145,146);
