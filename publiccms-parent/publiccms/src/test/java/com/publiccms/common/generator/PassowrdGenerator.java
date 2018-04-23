f5e2b6100afd4e5306f0c387382aa4f62921afef
sanluan <zyyy358@126.com>
2018-04-23 17:23:31 +0800

    删除数据库密码

--- publiccms-parent/publiccms/src/test/java/com/publiccms/common/generator/PassowrdGenerator.java
+++ publiccms-parent/publiccms/src/test/java/com/publiccms/common/generator/PassowrdGenerator.java
@@ -7,8 +7,8 @@ public class PassowrdGenerator {
     public static void main(String[] args) {
         String password = ""; // 这里输入加密前的密码
         System.out.println(VerificationUtils.base64Encode(VerificationUtils.encrypt(password, CommonConstants.ENCRYPT_KEY)));
-        // String encryptPassword = ""; // 这里输入加密后的密码
-        // System.out.println(VerificationUtils.decrypt(VerificationUtils.base64Decode(encryptPassword),
-        // CommonConstants.ENCRYPT_KEY));
+        String encryptPassword = ""; // 这里输入加密后的密码
+        System.out
+                .println(VerificationUtils.decrypt(VerificationUtils.base64Decode(encryptPassword), CommonConstants.ENCRYPT_KEY));
     }
 }
